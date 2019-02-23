b917a39c8bfa3bda7783bea032ed17f6fa19ed15
hoparkgo9ma <me@ho9.me>
2019-02-23 22:22:16 +0900

    init

---
+++ link.js
@@ -0,0 +1,12 @@
+const linkDict = {
+    'paradigm-shift' : 'https://html5up.net/uploads/demos/paradigm-shift/',
+    'massively' : 'https://html5up.net/uploads/demos/massively/',
+    'fractal' : 'https://html5up.net/uploads/demos/fractal/',
+    'spectral' : 'https://html5up.net/uploads/demos/spectral/',
+    'photon' : 'https://html5up.net/uploads/demos/photon/',
+    'highlights' : 'https://html5up.net/uploads/demos/highlights/'
+}
+
+function linktem (link) {
+    window.location = linkDict[link];
+}
